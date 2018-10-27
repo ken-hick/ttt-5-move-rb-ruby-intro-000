08f82a1d5cad189b6aafce70b3a69e077bcf0f7d
Ken Hick <hick.ken@gmail.com>
2018-10-27 10:31:08 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -16,7 +16,8 @@ def move(value = "X")
   def update_array_at_with(array, index, value)
       array[index] = value
   end
+  update_array_at_with(board, 0, "X")
+  board
 end
 
-update_array_at_with(board, 0, "X")
-board
+
